24ffa7969d7bd587d36422f397bfd59851b67ca7
Ahmed Ibrahim <ahmedibrahim@Ahmeds-MacBook-Pro.local>
2024-06-12 14:11:33 +0300

    initial

---
+++ TrySwiftUI/SwiftUI-New/SwiftUI-View-In-UIKIt.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUI-View-In-UIKIt.swift
+//  TrySwiftUI
+//
+//  Created by Ahmed Ibrahim on 12/06/2024.
+//
+
+import SwiftUI
+
+struct SwiftUI_View_In_UIKIt: View {
+    var body: some View {
+        VStack {
+            Text("hi")
+        }
+    }
+}
+
+#Preview {
+    SwiftUI_View_In_UIKIt()
+}
